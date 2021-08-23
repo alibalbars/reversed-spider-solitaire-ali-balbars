7ee86f49e478a5d6bbdd7e5901c7427da26eacc9
alibalbars <alibalbars@gmail.com>
2021-08-23 22:16:50 +0300

    Fix project title

--- cypress/integration/tests/load.spec.js
+++ cypress/integration/tests/load.spec.js
@@ -1,7 +1,16 @@
 // const herokuUrl = "https://reversed-spyder-solitaire.herokuapp.com/";
 
 describe("The Home Page", () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+
     it("successfully loads", () => {
         cy.visit('/');
     });
+
+    it('should have "reversed spider solitaire" title', () => {
+        const title = cy.title();
+        title.should('equal', "Reversed Spyder Solitaire")  
+    })
 });
